a5146137756ca71027f3b77157435ddf9ffba59a
zhf <732898328@qq.com>
2018-10-23 17:06:01 +0800

    del

--- sort/test/com/tzrough/sort/common/ArrayGenerator.java
+++ sort/test/com/tzrough/sort/common/ArrayGenerator.java
@@ -21,21 +21,4 @@ public class ArrayGenerator
 		return a;
 	}
 	
-	// 升序检查
-	public boolean checkSorted(int[] a) 
-	{
-		boolean flag = true;
-		
-		for (int i = 0; i < a.length - 1; i++)
-		{
-			if(a[i] > a[i+1]) 
-			{
-				flag = false;
-				break;
-			}
-		}
-		
-		return flag;
-	}
-	
 }
